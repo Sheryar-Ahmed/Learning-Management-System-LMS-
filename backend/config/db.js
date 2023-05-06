cbbd7ae087eb0971ac7d7a27981aa6f7436f7b18
Sheryar Ahmed <royalsheryar505@gmail.com>
2023-05-06 20:44:00 +0500

    console message

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const dbConn = async () => {
     try {
         console.log(process.env.MONGODB_URI);
         const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log("mongodb connected suceesfully.")
     } catch (err) {
         console.log(err);
         process.exit(1);
